9a070253e50424856caa5b643687d8edcef2b333
Heidi Cho <hcho5@wellesley.edu>
2019-11-24 15:41:11 -0500

    commiting changes to read page

--- public/scraper.py
+++ public/scraper.py
@@ -1,10 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+from requests.exceptions import ConnectionError
 
-num_articles = 20
+num_articles = 5
 
 def getTitle(url):
+    try:
+        # Getting the webpage, creating a Response object.
+        response = requests.get(url)
+
+        # Extracting the source code of the page.
+        data = response.text
+
+        # Passing the source code to BeautifulSoup to create a BeautifulSoup object for it.
+        soup = BeautifulSoup(data, 'lxml')
+
+        # Extracting all the <a> tags into a list.
+        if (soup.find('title') is not None):
+            return soup.find('title').text.replace('\n','')
+    except ConnectionError:
+        # print ('Failed to open: ' + url)
+        return ""
+
+def scrapeLinks(url, keyword, selector):
     # Getting the webpage, creating a Response object.
     response = requests.get(url)
 
@@ -15,42 +34,50 @@ def getTitle(url):
     soup = BeautifulSoup(data, 'lxml')
 
     # Extracting all the <a> tags into a list.
-    if (soup.find('title') is not None):
-        return soup.find('title').text
+    tags = soup.select(selector)
 
-def scrapeLinks(url, keyword):
-    # Getting the webpage, creating a Response object.
-    response = requests.get(url)
-
-    # Extracting the source code of the page.
-    data = response.text
-
-    # Passing the source code to BeautifulSoup to create a BeautifulSoup object for it.
-    soup = BeautifulSoup(data, 'lxml')
-
-    # Extracting all the <a> tags into a list.
-    tags = soup.find_all('a')
-
-    links = []
+    links = set()
     # Extracting URLs from the attribute href in the <a> tags.
     for tag in tags:
         href = tag.get('href')
         if (keyword in href):
-            links.append(href)
+            links.add(href)
 
-    return links
+    return list(links)
+
+def writeLinks(f, links):
+    i = 0
+    for link in links:
+        try:
+            if (i < num_articles):
+                title = getTitle(link)
+                if (title != ""):
+                    if (i == num_articles - 1):
+                        f.write(title + ", " + link)
+                    else:
+                        f.write(title + ", " + link + "\n")
+                    i += 1
+        except:
+            print ("CAUGHT ERROR")
+    f.close()
 
 # CNN
 cnnUrl = "https://www.cnn.com/specials/politics/2020-election-coverage"
-cnnLinks = scrapeLinks(cnnUrl, "politics")
+cnnLinks = scrapeLinks(cnnUrl, "politics", "a")
 cnnLinks = ["https://www.cnn.com" + link for link in cnnLinks]
 f = open("cnnLinks.txt", "w")
+writeLinks(f, cnnLinks)
 
-i = 0
-everyOther = 0
-for link in cnnLinks:
-    if (re.search('\d{4}[/.-]\d{2}[/.-]\d{2}', link)) and i < num_articles and everyOther%2 ==0:
-        f.write(getTitle(link) + ",, " + link + "\n")
-        i += 1
-    everyOther += 1
-f.close()
+# New York Times
+nytUrl = "https://www.nytimes.com/news-event/2020-election"
+nytLinks = scrapeLinks(nytUrl, "politics", "a")
+nytLinks = ["https://www.nytimes.com" + link for link in nytLinks]
+f = open("nytLinks.txt", "w")
+writeLinks(f, nytLinks)
+
+# Fox
+foxURL = "https://www.foxnews.com/category/politics/2020-presidential-election"
+foxLinks = scrapeLinks(foxURL, "", "article a")
+foxLinks = ["https://www.foxnews.com" + link for link in foxLinks]
+f = open("foxLinks.txt", "w")
+writeLinks(f, foxLinks)
